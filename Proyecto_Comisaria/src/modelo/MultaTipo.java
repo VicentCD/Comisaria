82f1bdc40699d859750798c33646db7c6afa2c03
daw1 <daw1@172.16.201.11>
2016-05-12 14:02:43 +0200

    v2.0

--- Proyecto_Comisaria/src/modelo/MultaTipo.java
+++ Proyecto_Comisaria/src/modelo/MultaTipo.java
@@ -1,5 +1,57 @@
 package modelo;
 
 public class MultaTipo {
+    private int id;
+    private String descripcion;
+    private double importe;
     private Tipo tipo; 
+    private int carnetpuntos;
+
+    public MultaTipo(int id, String descripcion, double importe, Tipo tipo, int carnetpuntos) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.importe = importe;
+        this.tipo = tipo;
+        this.carnetpuntos = carnetpuntos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCarnetpuntos() {
+        return carnetpuntos;
+    }
+
+    public void setCarnetpuntos(int carnetpuntos) {
+        this.carnetpuntos = carnetpuntos;
+    }
 }
