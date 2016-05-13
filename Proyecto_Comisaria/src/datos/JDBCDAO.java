2c6de2fe97f4fe2fa09bd7a194c65035afd424f0
DrCebria <gerardcebria@gmail.com>
2016-05-13 09:22:44 +0200

    Parque

--- Proyecto_Comisaria/src/datos/JDBCDAO.java
+++ Proyecto_Comisaria/src/datos/JDBCDAO.java
@@ -1,5 +1,22 @@
 package datos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class JDBCDAO {
+    private Connection conexion;
     
-}
+    public Connection CrearConexion(){
+        try {
+            this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/comisaria", "root", "root");
+
+        } catch (SQLException e) {
+            //LabelConexion.setText(e.getMessage() + " -- " + e.getErrorCode());
+            throw new RuntimeException(e);
+        }
+
+        return conexion;
+    }
+    }
