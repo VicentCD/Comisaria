b7168ba2de4ea6587aa5b41672f64ea86c2edb55
DrCebria <gerardcebria@gmail.com>
2016-05-13 09:35:54 +0200

    Parque

--- Proyecto_Comisaria/src/datos/JDBCDAO.java
+++ Proyecto_Comisaria/src/datos/JDBCDAO.java
@@ -2,13 +2,16 @@ package datos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import modelo.*;
 
 public class JDBCDAO {
+
     private Connection conexion;
-    
-    public Connection CrearConexion(){
+
+    public Connection CrearConexion() {
         try {
             this.conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/comisaria", "root", "root");
 
@@ -19,4 +22,14 @@ public class JDBCDAO {
 
         return conexion;
     }
+
+    public boolean introducirMulta(Multa m) throws SQLException {
+        boolean comprobacion = false;
+        PreparedStatement ps;
+        ResultSet rs;
+        ps = conexion.prepareStatement("INSERT INTO multas values ('?', '?', '?', '?', '?', '?', '?')");
+        
+        return comprobacion;
     }
+
+}
