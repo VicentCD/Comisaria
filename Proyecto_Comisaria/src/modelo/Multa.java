267920badb50ec9dfd44552c42a6098e71c5216a
christianbegines <christianbeginescarrasco@gmail.com>
2016-05-13 10:06:36 +0200

    packages

--- Proyecto_Comisaria/src/modelo/Multa.java
+++ Proyecto_Comisaria/src/modelo/Multa.java
@@ -1,5 +1,79 @@
 package modelo;
+import java.util.Date;
 
 public class Multa {
+    private int id;
+    private String descripcion;
+    private Date fecha;
+    private double importe;
+    private int idPolicia;
+    private String nifInfractor;
+    private int idTipo;
+
+    public Multa(int id, String descripcion, String fecha, double importe, int idPolicia, String nifInfractor, int idTipo) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.importe = importe;
+        this.idPolicia = idPolicia;
+        this.nifInfractor = nifInfractor;
+        this.idTipo = idTipo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public int getIdPolicia() {
+        return idPolicia;
+    }
+
+    public void setIdPolicia(int idPolicia) {
+        this.idPolicia = idPolicia;
+    }
+
+    public String getNifInfractor() {
+        return nifInfractor;
+    }
+
+    public void setNifInfractor(String nifInfractor) {
+        this.nifInfractor = nifInfractor;
+    }
+
+    public int getIdTipo() {
+        return idTipo;
+    }
+
+    public void setIdTipo(int idTipo) {
+        this.idTipo = idTipo;
+    }
     
 }
